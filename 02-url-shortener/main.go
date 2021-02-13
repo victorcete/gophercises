6539b0b0fc1271d7ac1c1c9d01aeac1f6638b6bf
Victor Lopez <victorcete@gmail.com>
2021-02-13 22:37:27 +0100

    read config from a file

--- 02-url-shortener/main.go
+++ 02-url-shortener/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	urlshort "github.com/victorcete/gophercises/02-url-shortener/handler"
 )
 
 func main() {
+	yamlConfig := flag.String("file", "config.yml", "YAML file with path and URL parameters")
+	flag.Parse()
+
+	yamlBytes, err := ioutil.ReadFile(*yamlConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -17,17 +28,10 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// Build the YAMLHandler using the MapHandler as the fallback
+	yamlHandler, err := urlshort.YAMLHandler(yamlBytes, mapHandler)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
